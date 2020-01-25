e5cdf0309000bcfeadf89e5c0ebcfa11dd810bc0
Lera Bilshchuk <leratheone@gmail.com>
2020-01-25 11:55:58 +0300

    Убирает лишние комментарии, добавляет некоторые константы и переменные

--- js/stat.js
+++ js/stat.js
@@ -10,9 +10,9 @@ var PADDING_TOP = 15;
 var PADDING_BOTTOM = 15;
 var PADDING_LEFT = 30;
 
-var BAR_WIDTH = 40;// ширина колонки
-var MAX_BAR_HEIGHT = 150;// высота гистограммы
-var BAR_GAP = 50;// расстояние между колонками
+var BAR_WIDTH = 40;
+var MAX_BAR_HEIGHT = 150;
+var BAR_GAP = 50;
 
 var PLAYER_NAME = 'Вы';
 var PLAYER_BAR_COLOR = 'rgb(255, 0, 0, 1)';
@@ -20,6 +20,8 @@ var PLAYER_BAR_COLOR = 'rgb(255, 0, 0, 1)';
 var FONT_SIZE = 16;
 var FONT_STYLE = 'PT Mono';
 var FONT_GAP = 5;
+
+var BLACK_COLOR = '#000000';
 
 window.renderStatistics = function (ctx, names, times) {
   if (names.length !== times.length) {
@@ -33,7 +35,7 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 'rgba(0, 0, 0, 0.7)', CLOUD_X + CLOUD_SHADOW_SIZE, CLOUD_Y + CLOUD_SHADOW_SIZE);
   renderCloud(ctx, '#ffffff', CLOUD_X, CLOUD_Y);
 
-  ctx.fillStyle = '#000000';
+  ctx.fillStyle = BLACK_COLOR;
 
   ctx.font = FONT_SIZE + 'px ' + FONT_STYLE;
   ctx.textBaseline = 'hanging';
@@ -43,7 +45,7 @@ window.renderStatistics = function (ctx, names, times) {
   var maxSpeed = getMaxNumber(times);
 
   for (var i = 0; i < names.length; i += 1) {
-    var barHeight = (MAX_BAR_HEIGHT * times[i]) / maxSpeed;// высота колонки текущего игрока
+    var barHeight = (MAX_BAR_HEIGHT * times[i]) / maxSpeed;
 
     if (names[i] === PLAYER_NAME) {
       ctx.fillStyle = PLAYER_BAR_COLOR;
@@ -51,17 +53,26 @@ window.renderStatistics = function (ctx, names, times) {
       ctx.fillStyle = 'hsl(240, ' + Math.round(Math.random() * 100) + '%, ' + Math.round(Math.random() * 100) + '%)';
     }
 
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - barHeight - PADDING_BOTTOM - FONT_SIZE - FONT_GAP, BAR_WIDTH, barHeight);
-    ctx.fillStyle = '#000000';
+    var barX = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
+    var barY = CLOUD_Y + CLOUD_HEIGHT - barHeight - PADDING_BOTTOM - FONT_SIZE - FONT_GAP;
+    ctx.fillRect(barX, barY, BAR_WIDTH, barHeight);
+    ctx.fillStyle = BLACK_COLOR;
 
-    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - barHeight - PADDING_BOTTOM - 2 * FONT_SIZE - 2 * FONT_GAP);
-    ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - PADDING_BOTTOM - FONT_SIZE);
+    var time = Math.round(times[i]);
+    var timeX = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
+    var timeY = CLOUD_Y + CLOUD_HEIGHT - barHeight - PADDING_BOTTOM - 2 * FONT_SIZE - 2 * FONT_GAP;
+    ctx.fillText(time, timeX, timeY);
+
+    var name = names[i];
+    var nameX = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
+    var nameY = CLOUD_Y + CLOUD_HEIGHT - PADDING_BOTTOM - FONT_SIZE;
+    ctx.fillText(name, nameX, nameY);
   }
 };
 
 var renderCloud = function (ctx, color, startX, startY) {
   ctx.fillStyle = color;
-  ctx.strokeStyle = '#000000';
+  ctx.strokeStyle = BLACK_COLOR;
   ctx.fillRect(startX, startY, CLOUD_WIDTH, CLOUD_HEIGHT);
   ctx.strokeRect(startX, startY, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
